9a198946034aefd6595e3b1b23f122915def38f6
JENNA EBOT <jennaagbor013@gmail.com>
2023-05-26 19:42:04 +0100

    fourth commit

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {BsWhatsapp} from 'react-icons/bs'
+import {FaTwitter} from 'react-icons/fa'
 
 const Contact = () => {
   return (
-    <section id = "contact">contact</section>
+    <section id = "contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className=" container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>jennaagbor013@gmail.com</h5>
+            <a href="mailto:jennaagbor013@gmail.com" target="_blank">Send a message</a>
+          </article>
+
+          <article className="contact__option">
+            <BsWhatsapp className='contact__option-icon'/>
+            <h4>Whatsapp</h4>
+            <h5>+237 671649793</h5>
+            <a href="https://api.whatsapp.com/send?phone=+237671649793" target="_blank">Send a message</a>
+          </article>
+
+          <article className="contact__option">
+            <FaTwitter className='contact__option-icon'/>
+            <h4>Twitter</h4>
+            <h5>agbor_jenna</h5>
+            <a href="https://www.twitter.com/agbor_jenna" target="_blank">Send a message</a>
+          </article>
+        </div>
+        <form action="">
+          <input type="text" name="Name" placeholder='Your full name' required />
+          <input type="email" name="email" placeholder='Your email' required />
+          <textarea name="message" id="" cols="7" placeholder='Your message' required></textarea>
+          <button type="submit" className='btn btn-primary'>Submit message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import './footer.css'
+import  {BsLinkedin} from 'react-icons/bs'
+import  {FaFacebookF} from 'react-icons/fa'
+import {FaTwitter} from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>JENNA</a>
+
+      <ul className="permalinks">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+      <a href='https://linkedin.com' target='blank'><BsLinkedin/> </a>
+      <a href='https://facebook.com' target='blank'><FaFacebookF/> </a>
+      <a href='https://twitter.com' target='blank'><FaTwitter/> </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; JENNA Portfolio. All rights reserved</small>
+      </div>
+    </footer>
   )
 }
 
