185cd8e2e335840d22f1694d0eac49e7e0ef9e83
JENNA EBOT <jennaagbor013@gmail.com>
2023-06-19 23:35:52 +0100

    first commit

---
+++ config/appconfig.js
@@ -0,0 +1,10 @@
+require('dotenv').config;
+
+const MONGO_DB_CONFIG = {
+
+  DB: 'mongodb+srv://jennaagbor013:tX67RrGaHnWxvXT6@cluster0.lnuwkyj.mongodb.net/ReactBackendendProject?retryWrites=true&w=majority'
+}
+
+module.exports = {
+  MONGO_DB_CONFIG
+}
